a3f5ce6858d62e332f34335e390de61ad7d81bef
erickoukos <erickoukos@gmail.com>
2023-02-04 09:28:38 +0300

    Completed form, check for centering the form at 75%

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
 
 </head>
 <body>
-    <nav class="navbar navbar-expand-sm navbar-light bg-light mb-2">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-2">
         <div class="container">
             <a class = "navbar-brand" href="#">FeedBack Limited</a>
             <button class="navbar-toggler" type = "button" data-bs-toggle="collapse" data-bs-target = "#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle Navigation">
--- index.php
+++ index.php
@@ -1,12 +1,44 @@
-<?php include 'includes/header.php';
-$user = "Sam";
-echo "<h1> Welcome ${user}</h1>";
+<?php include('includes/header.php'); ?>
+<?php
+// Create empty vars
+$names = $email = $body = '';
+$names_error = $email_error = $body_error = '';
+
+
 
 ?>
+<img src="images/Erick.png" alt="Logo" class = "w-25">
+<h2>Feedback Form</h2>
+<?php echo isset($names) ? $names : ''; ?>
+<p class = "lead text-center">Leave your feedback for PhP Course</p>
 
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class = "mt-4 w-75">
 
+<div class="mb-3">
+    <label for="names" class = "form-label">Your Names</label>
+    <input type="text" name="names" id="names" class = "form-control <?php echo !$names_error ? : 'is-invalid'; ?>" placeholder = "Enter your names" value = "<?php echo $names; ?>">
 
-<?php include 'includes/footer.php';
+<div class="invalid-feedback" id = "validationServerFeedback">
+    Please provide a valid name!
+</div>
+</div>
+
+<div class="mb-3">
+    <label for="email" class = "form-label">Your Email Address</label>
+    <input type="text" name="email" id="email" class = "form-control <?php echo !$email_error ? : 'is-invalid'; ?>" placeholder = "Enter your email" value = "<?php echo $email; ?>">
+</div>
+
+<div class="mb-3">
+    <label for="body">Feedback</label>
+    <textarea name="body" id="body" class = "form-control <?php echo !$body_error ? : 'is-invalid'; ?>" placeholder = "Enter your feedback" value = "<?php echo $body; ?>"></textarea>
+</div>
+
+<div class="mb-3">
+    <input type="submit" value="Send" name = "submit" class = "btn btn-dark w-100">
+</div>
+</form>
+
+<?php include('includes/footer.php');?>
 
 
 
